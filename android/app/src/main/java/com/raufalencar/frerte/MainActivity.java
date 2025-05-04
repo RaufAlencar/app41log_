18622dcc8fc7bfbc4a1629989bbe7f26e4e4e5d6
Your NameRaufALencar <rufalencar@gmail.com>
2025-05-04 18:28:39 -0300

    Preparando para gerar APK com Capacitor

---
+++ android/app/src/main/java/com/raufalencar/frerte/MainActivity.java
@@ -0,0 +1,5 @@
+package com.raufalencar.frerte;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/sw.js
@@ -0,0 +1,24 @@
+﻿// Service Worker básico (cache estático)
+const CACHE_NAME = 'app41log-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
